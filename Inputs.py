84ff3e49ac70e4ba73fadedac0b83eba93e0eca6
nexowo <jmaccou@outlook.fr>
2023-01-19 20:41:10 +0100

    New file

---
+++ Inputs.py
@@ -0,0 +1,48 @@
+import keyboard as key
+import time
+'''
+This file is used to define all the inputs the bot may need to use.
+Used to work with an AZERTY keyboard configuration. You may change the inputs depending on the one you use in your game.
+'''
+
+def bomb():
+    key.press_and_release('a')
+
+def activable():
+    key.press_and_release('space')
+
+def shoot_left():
+    key.press('left')
+
+def shoot_right():
+    key.press('right')
+
+def shoot_up():
+    key.press('up')
+
+def shoot_down():
+    key.press('down')
+
+def release_shoot():
+    key.release('left')
+    key.release('right')
+    key.release('up')
+    key.release('down')
+
+def go_left():
+    key.press('q')
+
+def go_right():
+    key.press('d')
+
+def go_up():
+    key.press('z')
+
+def go_down():
+    key.press('s')
+
+def release_dir():
+    key.release('z')
+    key.release('q')
+    key.release('s')
+    key.release('d')
